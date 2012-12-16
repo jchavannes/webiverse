4acb9f6bfeea066e917caebe1a89ee481c737281
Jason Chavannes <jason.chavannes@gmail.com>
2012-12-16 03:24:40 -0800

    Initial work done before git

---
+++ index.php
@@ -0,0 +1,272 @@
+<?php session_start(); ?>
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<title>Socket Cube</title>
+	<meta charset="utf-8">
+	<script type="text/javascript">var SESSIONID = document.cookie.match(/PHPSESSID=([^;]+)/)[1];</script>
+
+	<style type="text/css">
+	body {
+		background-color: #333;
+		margin: 0px;
+		overflow: hidden;
+	}
+	</style>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js"></script>
+	<script type="text/javascript" src="socket.io.js"></script>
+</head>
+<body>
+	<script type="text/javascript" src="Three.js"></script>
+	<script>
+	var Controls = new (function() {
+
+		this.keyDown = {
+
+			left: false,
+			right: false,
+			up: false,
+			down: false,
+			keyW: false,
+			keyA: false,
+			keyS: false,
+			keyD: false
+
+		}
+
+		$(document).keydown(function(e) {
+
+			if (e.keyCode == 37) { Controls.keyDown.left = true; }
+			else if (e.keyCode == 38) { Controls.keyDown.up = true; }
+			else if (e.keyCode == 39) { Controls.keyDown.right = true; }
+			else if (e.keyCode == 40) { Controls.keyDown.down = true; }
+			else if (e.keyCode == 87) { Controls.keyDown.keyW = true; }
+			else if (e.keyCode == 65) { Controls.keyDown.keyA = true; }
+			else if (e.keyCode == 83) { Controls.keyDown.keyS = true; }
+			else if (e.keyCode == 68) { Controls.keyDown.keyD = true; }
+			else { return; }
+
+			e.preventDefault();
+
+		}).keyup(function(e) {
+
+			if(e.keyCode == 37) { Controls.keyDown.left = false; }
+			else if(e.keyCode == 38) { Controls.keyDown.up = false; }
+			else if(e.keyCode == 39) { Controls.keyDown.right = false; }
+			else if(e.keyCode == 40) { Controls.keyDown.down = false; }
+			else if(e.keyCode == 87) { Controls.keyDown.keyW = false; }
+			else if(e.keyCode == 65) { Controls.keyDown.keyA = false; }
+			else if(e.keyCode == 83) { Controls.keyDown.keyS = false; }
+			else if(e.keyCode == 68) { Controls.keyDown.keyD = false; }
+
+		});
+
+	});
+	var Animate = new (function() {
+		
+		var camera, scene, renderer, cube = [], player = [], playerCords = [];
+		init();
+
+		function init() {
+			camera = new THREE.PerspectiveCamera(50, window.innerWidth/window.innerHeight, 0.1, 1000);
+			camera.position.z = 50;
+
+			scene = new THREE.Scene();
+			scene.add(camera);
+
+			var directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+			directionalLight.position.set(30, 30, 50);
+			directionalLight.rotation.x -= 90;
+			scene.add(directionalLight);
+
+			directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+			directionalLight.position.set(-30, 30, -50);
+			directionalLight.rotation.x += 90;
+			scene.add(directionalLight);
+
+			renderer = new THREE.WebGLRenderer();
+			renderer.setSize(window.innerWidth, window.innerHeight);
+			document.body.appendChild(renderer.domElement);
+
+			addCube();
+			addFloor();
+			setInterval(update, 1000/30);
+		}
+		function addFloor() {
+			var geometry = new THREE.CubeGeometry(500, 5, 1000);
+			var material = new THREE.MeshLambertMaterial({color: 0x556655});
+			var floor = new THREE.Mesh(geometry, material);
+			floor.position.y -= 40;
+			scene.add(floor);
+		}
+		function addCube() {
+			var geometry = new THREE.CubeGeometry(25, 25, 25);
+			var material = new THREE.MeshLambertMaterial({color: 0x3333dd});
+			var id = cube.length;
+			cube[id] = new THREE.Mesh(geometry, material);
+			scene.add(cube[id]);
+		}
+		function update() {
+			updateCube();
+			updateCamera();
+			updatePlayers();
+			renderer.render(scene, camera);
+		}
+		function updateCamera() {
+			var direction = (camera.rotation.y % 6) / 6 * 360;
+
+			if (Controls.keyDown.up || Controls.keyDown.keyW) {
+				camera.position.z -= 5*Animate.vectorZ(direction);
+				camera.position.x -= 5*Animate.vectorX(direction);
+			}
+			if (Controls.keyDown.down || Controls.keyDown.keyS) {
+				camera.position.z += 5*Animate.vectorZ(direction);
+				camera.position.x += 5*Animate.vectorX(direction);
+			}
+			if (Controls.keyDown.keyA) {
+				camera.position.z += 5*Animate.vectorZ(direction-90);
+				camera.position.x += 5*Animate.vectorX(direction-90);
+			}
+			if (Controls.keyDown.keyD) {
+				camera.position.z += 5*Animate.vectorZ(direction+90);
+				camera.position.x += 5*Animate.vectorX(direction+90);
+			}
+			if (Controls.keyDown.left) {
+				camera.rotation.y += 0.1;
+			}
+			if (Controls.keyDown.right) {
+				camera.rotation.y -= 0.1;
+			}
+		}
+		function updatePlayers() {
+			var max = player.length;
+			for (var i = 0; i < max; i++) {
+				if (typeof playerCords[i] != 'undefined' && playerCords[i].active) {
+					player[i].position.x = ((playerCords[i].cords.x - playerCords[i].startCords.x) / playerCords[i].steps) * (playerCords[i].steps - playerCords[i].stepsLeft) + playerCords[i].startCords.x;
+					player[i].position.z = ((playerCords[i].cords.z - playerCords[i].startCords.z) / playerCords[i].steps) * (playerCords[i].steps - playerCords[i].stepsLeft) + playerCords[i].startCords.z;
+					playerCords[i].stepsLeft--;
+					if (playerCords[i].stepsLeft == 0) {
+						playerCords[i].active = false;
+					}
+				}
+			}
+		}
+		function updateCube() {
+			var max = cube.length;
+			for (var i = 0; i < max; i++) {
+				cube[i].rotation.x += 0.1;
+				cube[i].rotation.y += 0.1;
+			}
+		}
+		this.vectorX = function(direction) {
+			return Math.sin(Math.PI * (direction/180));
+		}
+		this.vectorZ = function(direction) {
+			return Math.cos(Math.PI * (direction/180));
+		}
+		this.addPlayer = function(cords) {
+			var geometry = new THREE.CylinderGeometry(10, 25, 75);
+			var material = new THREE.MeshLambertMaterial({color: 0x3333dd});
+			var id = player.length;
+			player[id] = new THREE.Mesh(geometry, material);
+			playerCords[id] = {active:false, cords:cords, startCords:Animate.playerCords(id)};
+			player[id].position.x = cords.x;
+			player[id].position.z = cords.z;
+			scene.add(player[id]);
+			return id;
+		}
+		this.movePlayer = function(id, cords) {
+			playerCords[id] = {
+				active: true,
+				cords: cords,
+				startCords: Animate.playerCords(id),
+				steps: 6,
+				stepsLeft: 6
+			}
+		}
+		this.isPlayer = function(id) {
+			return typeof player[id] != 'undefined';
+		}
+		this.playerCords = function(id) {
+			if (typeof id == 'undefined') {
+				return {x:camera.position.x, z:camera.position.z, y:camera.rotation.y};
+			} else if (typeof player[id] != 'undefined') {
+				return {x:player[id].position.x, z:player[id].position.z};
+			}
+		}
+		this.setCamera = function(cords) {
+			camera.position.x = cords.x;
+			camera.position.z = cords.z;
+		}
+
+	})
+	var Socket = new (function() {
+		var user = {};
+		var players = [];
+		var init = function() {
+			socket = io.connect('ws://dev.socketgaming.com:8020');
+			socket.on('getSocket', function(data) {
+				cords = Animate.playerCords();
+				socket.emit('setSession', {sessionId:SESSIONID, sockId:data.sockId, x:cords.x, z:cords.z});
+			});
+			socket.on('getMyUserInfo', function(data) {
+				user.id = data.id;
+			});
+			socket.on('getMove', function(data) {
+				if (data.id == user.id) {
+					Animate.setCamera(data);
+					return;
+				}
+				var socketId = data.id;
+				if (!isPlayer(socketId)) {
+					addPlayer(socketId, data);
+				}
+				var animateId = getPlayer(socketId);
+				Animate.movePlayer(animateId, data);
+			});
+			socket.on('userExit', function(data) {
+				// Remove player
+			});
+			setInterval(checkPosition, 200);
+		}
+		function isPlayer(id) {
+			var id = getPlayer(id);
+			return id !== false;
+		}
+		function addPlayer(socketId, data) {
+			var id = players.length;
+			players[id] = {
+				socketId: socketId,
+				animateId: Animate.addPlayer({x:data.x, z:data.z})
+			}
+		}
+		function getPlayer(socketId) {
+			for (var i = 0; i < players.length; i++) {
+				if (players[i].socketId == socketId) {
+					return players[i].animateId;
+				}
+			}
+			return false;
+		}
+		this.getPlayer = getPlayer;
+		var lastPos;
+		function checkPosition() {
+			var pos = Animate.playerCords();
+			if (typeof lastPos == 'undefined' || pos.x != lastPos.x || pos.z != lastPos.z || pos.y != lastPos.y) {
+				lastPos = pos;
+				Socket.sendmove(pos);
+			}
+		}
+		this.sendmove = function(params) {
+			params.id = user.id;
+			socket.emit('sendMove', params);
+		}
+		init();
+	});
+	$(window).resize(function() {
+		window.location.reload();
+	})
+	</script>
+</body>
+</html>
---
+++ roomnavigate.node.js
@@ -0,0 +1,158 @@
+/* Author: Jason Chavannes <jason.chavannes@gmail.com>
+ * Date: 9/2/2012 */
+
+// Load dependencies and create data stores
+var io = require('socket.io').listen(8020);
+var sockets = [];
+var users = [];	
+var chatData = [];
+var checkPointsTimeout;
+
+// New connection
+io.sockets.on('connection', function (socket) {
+
+	// Add connection to socket store
+	var id = sockets.length;
+	sockets[id] = {id: id, socket: socket}
+
+	// Send socket id to client
+	sockets[id].socket.emit('getSocket', {sockId: id});
+
+	// Get session key from client
+	sockets[id].socket.on('setSession', function(data) {
+
+		// Save session key to socket store
+		sockets[data.sockId].sessionId = data.sessionId;
+		var userId = false;
+
+		// Check if user exists already
+		for(var i = 0; typeof users[i] != 'undefined'; i++) {
+			if(users[i].sessionId == data.sessionId) {
+				userId = i;
+				users[i].sockId = data.sockId;
+			}
+		}
+
+		// Create new user
+		if(userId === false) {
+			userId = users.length;
+
+			// Set detault information
+			users[userId] = {
+				id: userId,
+				sessionId: data.sessionId,
+				sockId: data.sockId,
+				active: true,
+				x: data.x,
+				z: data.z
+			}
+		}
+
+		// Save user id to socket store
+		sockets[data.sockId].userId = userId;
+
+		// Set / Refresh expiration
+		refreshUser(userId);
+
+		// Send user information to client
+		sockets[data.sockId].socket.emit('getMyUserInfo', {
+			id: userId
+		});
+
+		var now = new Date().getTime();
+		users.forEach(function(user) {
+
+			// Send all users to client
+			sockets[data.sockId].socket.emit('getMove', {
+				id: user.id,
+				x: user.x,
+				z: user.z
+			});
+
+			// Send new user to all clients
+			if(user.id != userId) {
+				sockets[user.sockId].socket.emit('getMove', {
+					id: users[userId].id,
+					x: users[userId].x,
+					z: users[userId].z
+				});
+			}
+		});
+	});
+
+	// Get move from client
+	sockets[id].socket.on('sendMove', function(data) {
+		if (typeof users[data.id] != 'undefined') {
+			refreshUser(data.id);
+			users[data.id].x = data.x;
+			users[data.id].z = data.z;
+
+			// Send move to all clients (except sending client)
+			users.forEach(function(user) {
+				if(user.id != data.id) {
+					sockets[user.sockId].socket.emit('getMove', {
+						id: data.id,
+						x: data.x,
+						z: data.z
+					})
+				}
+			});
+		}
+	});
+
+	// Make inactive on disconnect
+	var disconnect = sockets[id];
+	sockets[id].socket.on('disconnect', function() {
+		if(typeof users[sockets[disconnect.id].userId] != 'undefined') {
+			users[sockets[disconnect.id].userId].active = false;
+
+			// Send disconnect to all clients
+			users.forEach(function(user) {
+				if(user.active) {
+					sockets[user.sockId].socket.emit('userExit', {id: disconnect.userId});
+				}
+			});
+			sendMessage(sockets[disconnect.id].userId, "has disconnected.<br/>", false);
+		}
+	});
+});
+
+// Add message to history and send
+function sendMessage(id, msg, log) {
+	var name;
+	if(typeof log == 'undefined') {log = true;}
+	if(typeof users[id] != 'undefined' && typeof users[id].name != 'undefined') {
+		name = users[id].name;
+	} else {
+		name = "Server";
+		id = -1;
+	}
+	if(log) {chatData.push({id: id, msg: msg, name: name});}
+	users.forEach(function(user) {
+		if(user.active) {
+			sockets[user.sockId].socket.emit('newChat', {id: id, msg: msg, name: name});
+		}
+	});
+}
+
+// Update user expiration
+function refreshUser(userId) {
+	var now = new Date().getTime();
+	if(typeof users[userId] != 'undefined') {
+		users[userId].expire = now + 1000000;
+		users[userId].active = true;
+	}
+}
+
+// Get current race time
+function getTime(time) {
+	var min = parseInt(time/60000);
+	var sec = parseInt((time - min*60000)/1000);
+	var milli = ""+parseInt((time - sec*1000 - min*60000)/10);
+	if(min > 0) {
+		if(sec.length < 2) {sec = "0"+sec;}
+		sec = min+":"+sec;
+	}
+	if(milli.length < 2) {milli = "0"+milli;}
+	return ""+sec+"."+milli;
+}
